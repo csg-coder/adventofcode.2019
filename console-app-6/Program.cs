a862f7744f52a7e9740076e67d0aef5a43f01f28
csg <stefan.grigore@maxcode.net>
2019-12-06 10:47:15 +0200

    Add project files.

---
+++ console-app-6/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace console_app_6
+{
+    class Program
+    {
+        static Dictionary<string,List<string>> p;
+        static Dictionary<string,int> o;
+
+        static void Main(string[] args)
+        {
+        //    puzzle("stuff","input.txt");
+        //    puzzle("stuff","input2.txt");
+           puzzle("stuff","problem.txt");
+        }
+
+
+        static void puzzle(string descr,string input){
+            var dic=File.ReadLines(input).ToArray<string>();
+            System.Console.WriteLine(descr);
+            // print1(dic);
+
+            p=new Dictionary<string,List<string>>();
+            o=new Dictionary<string,int>();
+
+            p["COM"]=new List<string>();
+            o["COM"]=0;
+
+            foreach(var s in dic){
+                var vals=s.Split(")");
+                var around=vals[0];
+                var planet=vals[1];
+                if(!p.ContainsKey(around)){
+                    p[around]=new List<string>();
+                }
+                p[around].Add(planet);
+
+                if(o.ContainsKey(planet)){
+                    throw new Exception($"{planet} already there");
+                }
+                o[planet]=0;
+            }
+           
+            print2(o);
+            calc("COM",0);
+            
+            int total=0;
+            foreach(var item in o){
+                total+=item.Value;
+            }
+            System.Console.WriteLine(total);
+
+            show("YOU","SAN");
+        }
+
+        static int calc(string planet, int pos){
+            // System.Console.WriteLine(($"calc for {planet}").PadLeft(pos,'.'));
+            var cnt=0;
+             o[planet]=pos;
+            if(!p.ContainsKey(planet)){
+                // System.Console.WriteLine("final.."+planet);
+               
+                return 0;
+            }
+            foreach(var item in p[planet]){
+                //o[item]++;
+                cnt+=calc(item,pos+1)+1;
+            }
+            // System.Console.WriteLine($"{cnt} for {planet}");
+            return cnt;
+        }
+
+        static void show(string you,string san){
+            int max=0;
+            foreach(var item in o){
+                var cy=contains(item.Key,"YOU");
+                var cs=contains(item.Key,"SAN");
+                // System.Console.WriteLine($"{item.Key}:{cy}/{cs}");
+                if(max<item.Value && cy && cs)
+                    max=item.Value;
+            }
+
+            System.Console.WriteLine($"max is {max}; san is {o["SAN"]}; you is {o["YOU"]}");
+            System.Console.WriteLine($"result is "+(o["SAN"]-max+o["YOU"]-max-1-1));
+        }
+        static bool contains(string root,string planet){
+            // System.Console.WriteLine($"{root} contains {planet}?");
+            if(root==planet)
+                return true;
+            if(!p.ContainsKey(root)){
+                // System.Console.WriteLine("final.."+planet);
+               
+                return false;
+            }
+            foreach(var item in p[root]){
+                if(contains(item,planet))
+                    return true;
+                //o[item]++;
+            }
+            return false;
+        }
+ 
+        static void print2(Dictionary<string,int> a){
+            foreach(var item in a)
+                Console.WriteLine($"{item.Key}:{item.Value}");
+        }
+
+        static void print1(string[] a){
+            for(int i=0;i<a.Length-1;i++)
+                Console.Write($"{a[i]},");
+            Console.WriteLine(a[a.Length-1]);
+        }
+    }
+}
---
+++ console-app/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(respects(102));
+            System.Console.WriteLine(respects(112));
+            System.Console.WriteLine(respects(11124));
+            var cnt=0;
+            for(int i=136760;i<595730;i++){ 
+                var r=respects2(i);
+                System.Console.WriteLine($"{i}  - {r}");
+                if(r)cnt++;
+            }
+            System.Console.WriteLine($"cnt={cnt}");
+        }
+       static bool respects2(int n){
+            int prevcifra=n%10;
+
+            n=(n-prevcifra)/10;
+            var dup=false;
+            
+            var dupsize=1;
+
+            while(n>0){
+                
+                var cifra=n%10;
+
+                if(cifra>prevcifra)
+                    return false;
+
+                if(cifra!=prevcifra){
+                    if(dupsize==2)
+                        dup=true;
+                    dupsize=1;
+                }
+
+                if(cifra==prevcifra){
+                    dupsize++;
+                }
+
+                prevcifra=cifra;
+
+                n=(n-prevcifra)/10;
+            }
+            if(dupsize==2)
+                dup=true;
+            return dup;
+        }
+  
+       static bool respects(int n){
+            int prevcifra=n%10;
+
+            n=(n-prevcifra)/10;
+            var dup=false;
+
+            while(n>0){
+                var cifra=n%10;
+
+                if(cifra>prevcifra)
+                    return false;
+
+                if(cifra==prevcifra)
+                    dup=true;
+
+                prevcifra=cifra;
+
+                n=(n-prevcifra)/10;
+            }
+
+            return dup;
+        }
+  
+    }
+}
