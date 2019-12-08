64aca28b154ec66189a45197f5eaf27c76321ae5
Ciprian Grigore <cgrigore@gmail.com>
2019-12-08 19:03:58 +0200

    day 8

---
+++ day-8/Program2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace day_8
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+            //var input = File.ReadAllText("input-test.txt");
+
+            var lmax = 6; var cmax = 25;
+            //var lmax = 2; var cmax = 2;
+
+            char[,] img = new char[lmax, cmax];
+            for (int l = 0; l < lmax; l++)
+                for (int c = 0; c < cmax; c++)
+                {
+                    img[l, c] = '-';
+                }
+            int pos = 0;
+            while (pos < input.Length)
+            {
+                var r = input.Substring(pos, cmax * lmax);
+
+                for (int l = 0; l < lmax; l++)
+                    for (int c = 0; c < cmax; c++)
+                    {
+                        if (img[l, c] != '0' && img[l, c] != '1')
+                            img[l, c] = r[l * cmax + c];
+                    }
+
+                pos += cmax * lmax;
+            }
+            show(lmax, cmax, img);
+            draw(lmax, cmax, img);
+            //EJRGP
+        }
+
+        private static void show(int lmax, int cmax, char[,] img)
+        {
+            for (int l = 0; l < lmax; l++)
+            {
+                for (int c = 0; c < cmax; c++)
+                {
+                    if (img[l, c] == '0')
+                        Console.Write(' ');
+                    else
+                        Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+        private static void draw(int lmax, int cmax, char[,] img)
+        {
+            using (var bmp = new Bitmap(cmax, lmax))
+            using (var gr = Graphics.FromImage(bmp))
+            {
+                for (int l = 0; l < lmax; l++)
+                {
+                    for (int c = 0; c < cmax; c++)
+                    {
+                        bmp.SetPixel(c, l, img[l, c] == '1' ? Color.Black : Color.White);
+                    }
+                }
+                var path = "out.png";
+                bmp.Save(path);
+            }
+        }
+    }
+}
