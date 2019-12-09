c6a075f531be6a11d1574c9bd410e4067d1038e0
Ciprian Grigore <cgrigore@gmail.com>
2019-12-09 21:21:49 +0200

    day 9 it works

---
+++ day-9/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day_9
+{
+    public static class Utils
+    {
+        public static void print(this long[] ar, int start, int maxlength)
+        {
+            return;
+            Console.Write("\n>");
+            for (int i = start; i < maxlength + start; i++)
+            {
+                Console.Write("{0,5}:", ar[i]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
