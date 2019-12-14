758226d76a958c155e451bd0d9b5d5fc49ed1286
csg <cgrigore@gmail.com>
2019-12-14 10:21:57 +0200

    1st problem works

--- day-14/Program.cs
+++ day-14/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Xunit;
 
 namespace day_14
 {
@@ -15,16 +16,62 @@ namespace day_14
 
         static void Main(string[] args)
         {
-            var input = "sample1.txt";
-            int count = Solve(input);
-            Console.WriteLine(count);
+            Assert.Equal(31, Solve("sample1.txt"));
+            Assert.Equal(165, Solve("sample2.txt"));
+            Assert.Equal(13312, Solve("large-sample1.txt"));
+            Assert.Equal(180697, Solve("large-sample2.txt"));
+            Assert.Equal(2210736, Solve("large-sample3.txt"));
+            Assert.Equal(374457, Solve("input.txt"));
         }
+
+        static Dictionary<string, int> Raw { get; set; }
+        static Dictionary<string, Reaction> reactions { get; set; }
+        static int ore;
 
         private static int Solve(string input)
         {
-            Dictionary<string, Reaction> reactions = Read(input);
-            int count = reactions.Count;
-            return count;
+            reactions = Read(input);
+            Raw = new Dictionary<string, int>();
+            foreach (var compound in reactions)
+            {
+                Raw[compound.Key] = 0;
+            }
+
+            ore = 0;
+
+            Consume("FUEL", 1);
+            Console.WriteLine(ore);
+            return ore;
+        }
+
+        static void Reaction(string compound)
+        {
+            foreach (var raw in reactions[compound].Raw)
+            {
+                if (raw.Key == "ORE")
+                    ore += raw.Value;
+                else
+                {
+                    Consume(raw.Key, raw.Value);
+                }
+            }
+            //produced
+            Raw[compound] += reactions[compound].Quantity;
+
+        }
+
+        static void Consume(string compound, int quantity)
+        {
+            //Produce while not not have enough
+            while (quantity > Raw[compound])
+            {
+                Reaction(compound);
+            }
+
+            //Consume
+            Raw[compound] -= quantity;
+            return;
+
         }
 
         private static Dictionary<string, Reaction> Read(string input)
