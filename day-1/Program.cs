460416e9e0639805e937061fd63c28176cf93608
csg <cgrigore@gmail.com>
2019-12-23 21:36:29 +0200

    1st problem solved

---
+++ day-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Xunit;
+using System.Linq;
+
+namespace day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = File.ReadAllLines("input.txt").Select(o => int.Parse(o));
+
+            Func<int, int> fuel = (int mass) => { return (int)Math.Truncate(mass / 3.0) - 2; };
+            Assert.Equal(33583, fuel(100756));
+
+            var res=numbers.Select(o => fuel(o)).Sum();
+            Assert.Equal(3550236, res);
+            Console.WriteLine(res);
+        }
+    }
+}
